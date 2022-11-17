c62579b6987652e2f4b85c61ca073d64d99b802c
Nate <itsnzte@gmail.com>
2022-11-16 17:43:38 -0800

    added index file to pages directory, to use easy throughout application

---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+// Core Page Components File to Export as bundle
+
+// Import
+import About from "./About/AboutPage";
+import Discover from "./About/Discover";
+import Home from "./About/Home";
+import Profile from "./About/Profile";
+import Settings from "./About/Settings";
+
+// Export
+export {
+  About,
+  Discover,
+  Home,
+  Profile,
+  Settings
+};

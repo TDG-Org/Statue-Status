d865343bbc18d2dddae1dc7688f63bc0086688da
Nate <itsnzte@gmail.com>
2022-11-16 17:41:14 -0800

    added index to components to use easy in other parts of application

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+// Core Components File to Export as bundle
+
+// Import 
+import Aside from "./Aside";
+import Footer from "./Footer";
+import Model from "./Model";
+import Nav from "./Nav";
+
+// Export 
+export {
+  Aside, Footer, Model, Nav
+};

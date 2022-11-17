edeeffab443b3f5aa696cdcf747877f005bee90b
Nate <itsnzte@gmail.com>
2022-11-16 17:11:04 -0800

    added swal and bootstrap to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+// Sweet Alert 
+import swal from "sweetalert";
+
+// Bootstrap 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   return (
     <div className="App">

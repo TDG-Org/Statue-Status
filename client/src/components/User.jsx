abab6ebbb1061be2f67910a113eb7d2fee3a083c
Nate <itsnzte@gmail.com>
2022-11-25 07:46:00 -0800

    completed html layout for user component

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,7 +4,8 @@ const User = () => {
   return (
     <div className="user-avatar-section">
 
-      <div className="user-avatar-section-top">
+      {/* Top Section  */}
+      <div className="user-avatar-sect-top">
 
       {/* badge section  */}
       <ul className="badge-sect">
@@ -20,6 +21,26 @@ const User = () => {
 
       </div>
 
+      {/* Bottom Section  */}
+      <div className="user-avatar-sect-bottom">
+
+        {/* Username  */}
+        <h3 className="username" id="username">Nate</h3>
+        
+        {/* User bio section  */}
+        <p className="user-bio" id="userBio">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam saepe, dolor dolorem pariatur odit rem?
+        </p>
+
+        {/* User's Followers and Following Count  */}
+        <div className="follow-count-wrapper">
+          <p className="following-count"><span id="userFollowersCount">10k</span> Followers</p>
+          <p className="following-count"><span id="userFollowingCount">15k</span> Following</p>
+        </div>
+
+        {/* Link  */}
+
+      </div>
 
 
     </div>

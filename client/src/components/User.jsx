b888e4cf8fed457017293fd0506ae2e170d40e0b
Nate <itsnzte@gmail.com>
2022-11-25 07:38:53 -0800

    update user component

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,6 +4,24 @@ const User = () => {
   return (
     <div className="user-avatar-section">
 
+      <div className="user-avatar-section-top">
+
+      {/* badge section  */}
+      <ul className="badge-sect">
+        <li className="badge">
+
+        </li>
+      </ul>
+
+      {/* Avatar Section  */}
+      <div className="avatar-sect">
+        <img src="" alt="" />   
+      </div>
+
+      </div>
+
+
+
     </div>
   );
 };

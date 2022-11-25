f21eeb922931eec84632674d16da57b2bae7c5b3
Nate <itsnzte@gmail.com>
2022-11-25 07:48:13 -0800

    update footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,27 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      {/* Logo  */}
+      <div className="logo">
+        <h3>Statue Status</h3>
+      </div>
+
+      {/* Links  */}
+      <ul className="footer-links">
+        <li>
+          <a href="#">Extra</a>
+        </li>
+        <li>
+          <a href="#">Extra</a>
+        </li>
+        <li>
+          <a href="#">Extra</a>
+        </li>
+      </ul>
+
+    </footer>
   );
 };
 

1bef3c31088188f7bfb384db60c1b841a685f7ff
Nate <itsnzte@gmail.com>
2022-11-27 05:16:17 -0800

    update package json file

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import React from "react";
 // Wrapper 
 import { StatueStatus } from "./pages";
 
-// Sweet Alert 
-import swal from "sweetalert";
-
 // Bootstrap 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./sass/main.scss";
 
 function App() {
   return (
 
     // Statue Status Wrapper 
     <StatueStatus />
+
   );
 }
 

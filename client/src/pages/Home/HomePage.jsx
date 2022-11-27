123ffecda246181422b7598f740c8a7498e1b88c
Nate <itsnzte@gmail.com>
2022-11-27 10:32:51 -0800

    added alert styles

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -4,9 +4,20 @@ import React from "react";
 import "./HomePage.scss";
 
 const HomePage = () => {
+
+  let today = new Date();
+
   return (
     <div className="HomePage page">
-      HomePage
+      <h1>Wednesday, 12 2022</h1>
+
+      <div className="ss-alert">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum corporis eaque alias consectetur voluptas. Iste at suscipit rem, aperiam laborum beatae eum placeat numquam doloremque illo nulla ducimus, magnam quasi.
+      </div>
+
+
+
+
     </div>
   );
 };

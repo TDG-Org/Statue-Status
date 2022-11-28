9dcdd7e90969f7c15adaf746645817683721d5d2
Nate <itsnzte@gmail.com>
2022-11-28 14:07:59 -0800

    updated discover page

--- client/src/pages/Discover/DiscoverPage.jsx
+++ client/src/pages/Discover/DiscoverPage.jsx
@@ -7,22 +7,44 @@ const DiscoverPage = () => {
   return (
     <div className="DiscoverPage page">
       <div className="container">
+
         <h1>Discover</h1>
 
         <div className="discover-content">
 
           {/* Discovering Search Section  */}
-          <div className="discover-search-sect">
+          <div className="discover-search-sect pallet">
+            <h3>Explore</h3>
+
+            {/* Search Tool  */}
+            <div className="discover-search-tool">
+             <input type="text" className="search-bar" />
+              <button className="search-bar-btn"><i className="bi-alarm"></i></button>
+            </div>
+
+            {/* Search Results  */}
+            <div className="discover-search-results">
+              <div className="discover-result">
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+            </div>
 
           </div>
 
           {/* User's Friends  */}
-          <div className="discover-user-friends">
-            
+          <div className="discover-user-friends pallet">
+            <h3>Friends</h3>
+
+            {/* List of friends  */}
+            <ul className="friends-list">
+              <li>
+                <div className="friend-name">john</div>
+              </li>
+            </ul>
           </div>
 
         </div>
-        </div>
+      </div>
     </div>
   );
 };

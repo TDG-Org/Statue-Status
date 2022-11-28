6abcc60b2cdce646d42d9c0b0dfc2a9c99ac2a90
Nate <itsnzte@gmail.com>
2022-11-28 13:55:49 -0800

    adjusting discover page

--- client/src/pages/Discover/DiscoverPage.jsx
+++ client/src/pages/Discover/DiscoverPage.jsx
@@ -6,7 +6,23 @@ import "./DiscoverPage.scss";
 const DiscoverPage = () => {
   return (
     <div className="DiscoverPage page">
-      DiscoverPage
+      <div className="container">
+        <h1>Discover</h1>
+
+        <div className="discover-content">
+
+          {/* Discovering Search Section  */}
+          <div className="discover-search-sect">
+
+          </div>
+
+          {/* User's Friends  */}
+          <div className="discover-user-friends">
+            
+          </div>
+
+        </div>
+        </div>
     </div>
   );
 };

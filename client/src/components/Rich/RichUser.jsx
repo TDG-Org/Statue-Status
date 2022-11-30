df4ccdbc17b5b9f8428808dd78fc42143923b20e
Nate <itsnzte@gmail.com>
2022-11-30 02:47:51 -0800

    successfully transfered data to child component

--- client/src/components/Rich/RichUser.jsx
+++ client/src/components/Rich/RichUser.jsx
@@ -27,16 +27,17 @@ const RichUser = ({rank, name, money, country, image}) => {
 
       {/* User Left side  */}
       <div className="top-user-left">
-        <span className="top-user-rank">#<span className="top-user-rank-value">1</span></span>
-        <img src="https://thumbor.forbes.com/thumbor/200x200/smart/filters:format(jpeg)/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F62d700cd6094d2c180f269b9%2F416x416.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D959%26cropY1%3D0%26cropY2%3D959" alt="top user" className="top-user-avatar" />
-        <span className="top-user-name">Elon Musk</span>
+        <span className="top-user-rank">#<span className="top-user-rank-value">{rank}</span></span>
+        <img src={image} alt="top user" className="top-user-avatar" />
+        <span className="top-user-name">{name}</span>
       </div>
 
       {/* User Right side  */}
       <div className="top-user-right">
-        <span className="top-user-country">United States</span>
-        <span className="top-user-money"> $<span>100,000,000,000</span></span>
-        </div>
+        <span className="top-user-country">{country}</span>
+        <span className="top-user-money"> $<span>{money}</span></span>
+      </div>
+
   </div>
   );
 };

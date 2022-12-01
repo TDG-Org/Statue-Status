f3acaf8931fa9c5b1f339014764934f1856b60e7
Nate <itsnzte@gmail.com>
2022-12-01 13:13:50 -0800

    small update to test data

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -12,11 +12,10 @@ import "./HomePage.scss";
 // temp 
 const data = [
   {rank: 1, name: "Nate M", money: 43000, country: "United States", image: "https://natemci.com/static/media/Nate1.8cbd5164f1a9ecaea636.png"},
-  {rank: 2, name: "Luke M", money: 58430, country: "United States", image: "https://natemci.com/static/media/insomnia.ce01d16f9e95615eacda.png"},
-  {rank: 3, name: "Cesar I", money: 49864, country: "United States", image: "aaaa"},
+  {rank: 2, name: "Luke M", money: 67430, country: "United States", image: "https://natemci.com/static/media/insomnia.ce01d16f9e95615eacda.png"},
+  {rank: 3, name: "Cesar I", money: 49864, country: "United States", image: "https://media.discordapp.net/attachments/714743549962223697/1047983067412828230/7f126b252e2c8e8ebb546ab9d9050e85.png"},
   {rank: 4, name: "Gil E", money: 49718, country: "United States", image: "https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop"},
-  {rank: 5, name: "Tony Q", money: 49135, country: "United States", image: "helaaaaao"},
-  // {rank: 6, name: "Hello", money: 29135, country: "United States", image: "testing"}
+  {rank: 5, name: "Tony Q", money: 49135, country: "United States", image: "https://avatars.githubusercontent.com/u/99701688?v=4"}
 ];
 
 // Funtion that sorts Data my property (money) 

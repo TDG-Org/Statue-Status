cf5a3afb8ce85834b47f75fcf205f4576435861f
TDGLU <tdglukealt@gmail.com>
2022-12-01 14:04:53 -0800

    added default profile pictures and logo

--- client/src/assets/imgs/index.js
+++ client/src/assets/imgs/index.js
@@ -4,3 +4,6 @@ export const Brilliance = require("./brilliance.png");
 export const Bravery = require("./bravery.png");
 export const Pancake = require("./pancake.png");
 export const Planet = require("./blue-planet.png");
+export const Male = require("./male.png");
+export const Female = require("./female.png");
+export const Logo = require("./logo.png");

cfd071af4b6e0528ce15c276e1720bcce1ee7578
Nate <itsnzte@gmail.com>
2022-12-01 18:29:31 -0800

    update discover page for mobile devices

--- client/src/pages/Discover/DiscoverPage.jsx
+++ client/src/pages/Discover/DiscoverPage.jsx
@@ -10,50 +10,48 @@ import Search from "../../assets/svgs/search.svg";
 const DiscoverPage = () => {
   return (
     <div className="DiscoverPage page">
-      <div className="container">
 
-        <h1>Discover</h1>
+      <h1>Discover</h1>
 
-        <div className="discover-content">
+      <div className="discover-content">
 
-          {/* Discovering Search Section  */}
-          <div className="discover-search-sect pallet">
-            <h3>Explore</h3>
+        {/* Discovering Search Section  */}
+        <div className="discover-search-sect pallet">
+          <h4>Explore</h4>
 
-            {/* Search Tool  */}
-            <div className="discover-search-tool">
-             <input type="text" className="search-bar" placeholder="John Doe" />
-              <button className="search-bar-btn"><i className="bi-search"></i></button>
-            </div>
-
-            {/* Search Results  */}
-            <div className="discover-search-results">
-              <div className="discover-result">
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-            </div>
-
-            <img src={Search} alt="search" className="search-svg" />
-
+          {/* Search Tool  */}
+          <div className="discover-search-tool">
+          <input type="text" className="search-bar" placeholder="John Doe" />
+            <button className="search-bar-btn"><i className="bi-search"></i></button>
           </div>
 
-          {/* User's Friends  */}
-          <div className="discover-user-friends pallet">
-            <h3>Friends</h3>
-
-            {/* List of friends  */}
-            <ul className="friends-list">
-              <li className="friend">
-                <div className="friend-left">
-                  <img src={Planet} alt="" className="friend-avatar" />
-                  <p className="friend-username">Luke</p>
-                </div>
-                <button className="view-friend"><i className="bi bi-three-dots-vertical"></i></button>
-              </li>
-            </ul>
+          {/* Search Results  */}
+          <div className="discover-search-results">
+            <div className="discover-result">
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
           </div>
+
+          <img src={Search} alt="search" className="search-svg" />
 
         </div>
+
+        {/* User's Friends  */}
+        <div className="discover-user-friends pallet">
+          <h4>Friends</h4>
+
+          {/* List of friends  */}
+          <ul className="friends-list">
+            <li className="friend">
+              <div className="friend-left">
+                <img src={Planet} alt="" className="friend-avatar" />
+                <p className="friend-username">Luke</p>
+              </div>
+              <button className="view-friend"><i className="bi bi-three-dots-vertical"></i></button>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   );

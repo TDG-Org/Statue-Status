5d04120ce94ec67699487a9314365a84cb4189d0
Nate <itsnzte@gmail.com>
2022-12-02 00:00:29 -0800

    added layout for settings page

--- client/src/pages/Settings/SettingsPage.jsx
+++ client/src/pages/Settings/SettingsPage.jsx
@@ -6,7 +6,68 @@ import "./SettingsPage.scss";
 const SettingsPage = () => {
   return (
     <div className="SettingsPage page">
-      SettingsPage
+      <h1>Settings</h1>
+      
+      <hr />
+
+      {/* Profile Section  */}
+      <div className="profile-sect">
+        <h3>Edit Profile</h3>
+
+        <div className="pallet">
+
+          {/* Avatar  */}
+          <h4>Avatar</h4>
+          <div className="profile-sect-avatar">
+
+          </div>
+
+          {/* Username  */}
+          <h4>Username</h4>
+          <div className="profile-sect-name">
+
+          </div>
+
+          {/* Bio  */}
+          <h4>Bio</h4>
+          <div className="profile-sect-bio">
+
+          </div>
+
+        </div>
+
+      </div>
+
+      {/* Account Section  */}
+      <div className="account-sect">
+        <h3>My Account</h3>
+
+        <div className="pallet">
+
+          {/* Appearance  */}
+          <h4>Appearance</h4>
+
+          {/* Password  */}
+          <h4>Password</h4>
+
+        </div>
+
+      </div>
+
+      {/* General Settings  */}
+      <div className="settings-sect">
+        <h3>General</h3>
+
+        <div className="pallet">
+
+          <h4>Test</h4>
+          
+          <h4>Test</h4>
+
+        </div>
+
+      </div>
+
     </div>
   );
 };

85afa0bd9f825119903d358209c61bcede3b56b4
Nate <itsnzte@gmail.com>
2022-12-03 00:58:08 -0800

    fixing layout for profile section

--- client/src/pages/Profile/ProfilePage.jsx
+++ client/src/pages/Profile/ProfilePage.jsx
@@ -3,6 +3,9 @@ import React from "react";
 // Styles
 import "./ProfilePage.scss";
 
+// Images/SVGs 
+import { Pancake } from "../../assets/imgs/";
+
 const ProfilePage = () => {
   return (
     <div className="ProfilePage page">
@@ -19,22 +22,34 @@ const ProfilePage = () => {
 
           {/* Avatar  */}
           <div className="profile-sect-avatar">
-            <h4>Avatar</h4>
+            <div className="profile-sect-avatar-content">
+              <img src={Pancake} alt="" />
+              <div className="avatar-edit-overlay"></div>
+            </div>
+            <h4>Edit Avatar</h4>
           </div>
 
-          {/* Username  */}
-          <div className="profile-sect-name">
-            <h4>Username</h4>
-          </div>
+          <div className="text-edit-side">
+            {/* Username  */}
+            <div className="profile-sect-name">
+              <h4>Username</h4>
+            </div>
 
-          {/* Bio  */}
-          <div className="profile-sect-bio">
-            <h4>Bio</h4>
+            {/* Bio  */}
+            <div className="profile-sect-bio">
+              <h4>Bio</h4>
+            </div>
           </div>
 
         </div>
 
       </div>
+
+      {/* Statue Section  */}
+      <div className="statue-sect">
+
+      </div>
+
     </div>
   );
 };

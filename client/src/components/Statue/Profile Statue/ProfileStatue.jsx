097782d42f4a0dec3699652d600b792cb8dea343
Nate <itsnzte@gmail.com>
2022-12-06 00:07:57 -0800

    finally fixed all bugs and got the element to update and display correctly

--- client/src/components/Statue/Profile Statue/ProfileStatue.jsx
+++ client/src/components/Statue/Profile Statue/ProfileStatue.jsx
@@ -18,16 +18,35 @@ const ProfileStatue = () => {
   
   // edit name 
   const [editStatueName, setEditStatueName] = useState("John Doe");
-  
-  function handleToggleStatueName() {
-    setEditStatueNameActive(!editStatueNameActive);
 
+  // Current Statue Name input 
+  const [statueNameCurrent, setStatueNameCurrent] = useState("hi");
+  
+  // Toggle function to activate and let users edit statue name 
+  function handleToggleStatueName(e) {
+    setEditStatueNameActive(!editStatueNameActive);
     setTimeout(() => {
       statueNameRef.current.focus();
-    }, 100);
+    }, 50);
+  } 
+
+  // Update the input text 
+  function updateStatueName(e) {
+    console.log(statueNameCurrent);
+    setEditStatueName(statueNameCurrent);
+  }
+
+  function handleStatueNameInputChange(e) {
+    const target = e.target;
+    let value = target.value;
+    setStatueNameCurrent(value);
   }
 
 
+  // Update the display 
+  function displayStatueName() {
+    document.querySelector(".statue-name").value = editStatueName;
+  }
 
   return (
      <div className="statue-sect">
@@ -46,20 +65,46 @@ const ProfileStatue = () => {
               <img src={NatePfp} alt="" className="statue-img" />
               
              {/* Statue Name */}
-              <div className={`statue-name-sect ${editStatueNameActive ? "active" : ""}`}>
+              <div className="statue-name-sect">
                 <input
                   type="text"
                   className="statue-name"
-                  value={editStatueName}
+                  defaultValue={editStatueName} 
                   disabled={editStatueNameActive ? false : true}
                   ref={statueNameRef}
-                  />
+                  onChange={handleStatueNameInputChange} 
+                />
+                {/* Edit button  */}
                 <button
-                  className="statue-name-edit"
+                  className={`statue-name-edit ${editStatueNameActive ? "hide" : ""}`}
                   onClick={handleToggleStatueName}
                 >
                    <i className="bi bi-pen"></i>
                 </button>
+                <div className="statue-name-edit-btns">
+                  {/* Save button  */}
+                  <button
+                    className={`statue-name-save ${editStatueNameActive ? "" : "hide"}`}
+                    onClick={(e) => {
+                      updateStatueName();
+                      handleToggleStatueName(); 
+                    }}
+                  >
+                    Save
+                  </button>
+                  {/* Cancel button  */}
+                  <button
+                    className={`statue-name-save ${editStatueNameActive ? "" : "hide"}`}
+                    onClick={() => {
+                      handleToggleStatueName();
+                      displayStatueName();
+                    }
+                    }
+                  >
+                    Cancel
+                  </button>
+                </div>
+
               </div>
 
               <hr />

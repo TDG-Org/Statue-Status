7f755a974181a7612b0e05fa311281ba2756c44f
Nate <itsnzte@gmail.com>
2022-12-08 09:18:48 -0800

    updated social list on the second side of the profile statue

--- client/src/components/Statue/Profile Statue/ProfileStatueSecondary.jsx
+++ client/src/components/Statue/Profile Statue/ProfileStatueSecondary.jsx
@@ -12,93 +12,122 @@ const ProfileStatueSecondary = () => {
           <a
             href=""><i className="bi bi-instagram"></i> Instagram/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
         </li>
         <li>
           <a
             href=""><i className="bi bi-github"></i> Github/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-facebook"></i> Facebook/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-envelope"></i> Email/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-snapchat"></i> Snapchat/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-twitter"></i> Twitter/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-tiktok"></i> Tiktok/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-youtube"></i> Youtube/
             <span className="social-username">TDGNate</span>
-          </a> 
+            </a> 
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-linkedin"></i> LinkedIn/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-discord"></i> Discord/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-reddit"></i> Reddit/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-spotify"></i> Spotify/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-steam"></i> Steam/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-stack-overflow"></i>
             Stack overflow/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
         <li>
           <a
             href=""><i className="bi bi-pinterest"></i>
             Pinterest/
             <span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
 
         {/* Extra Links  */}
@@ -107,7 +136,9 @@ const ProfileStatueSecondary = () => {
             className="social-extra-link"
             href=""><i className="bi bi-link-45deg"></i>
             <span className="social-extra-link-platform">Platform</span>/<span className="social-username">TDGNate</span>
-          </a>
+            </a>
+            <i className="bi bi-x-lg statue-social-x"></i>
+            
         </li>
 
         {/* The More Socials button  */}

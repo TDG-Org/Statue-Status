2817c86b1c9fb631bb87e3141b5edb7ddd0be70e
Nate <itsnzte@gmail.com>
2022-12-12 18:44:46 -0800

    added people and styling the connect statue peers

--- client/src/components/Statue/Profile Statue/Secondary/ProfileStatueConnections.jsx
+++ client/src/components/Statue/Profile Statue/Secondary/ProfileStatueConnections.jsx
@@ -3,25 +3,74 @@ import React, { useState, useRef } from "react";
 // Proptypes 
 import PropTypes from "prop-types";
 
+import { Pancake, Brilliance, Bravery, NatePfp } from "../../../../assets/imgs";
+
 const ProfileStatueConnections = () => {
   return (
     <div className="secondary-sect-connections">
       <h4>Connections</h4>
 
-      <div className="statue-peer">
+      <div className="statue-peer-wrapper">
+{/* peer  */}
+<div className="statue-peer">
         {/* Image  */}
         <div className="statue-peer-img-wrapper">
-          <img src="" alt="" />
+          <img src={Brilliance} alt="" />
         </div>
         {/* Name  */}
         <div className="statue-peer-name-wrapper">
-          <p className="statue-peer-name">
+          <a href="" className="statue-peer-name">
             Luke McIlvenny
-          </p>
+          </a>
         </div>
         {/* The X button  */}
       </div>
 
+      {/* peer  */}
+      <div className="statue-peer">
+        {/* Image  */}
+        <div className="statue-peer-img-wrapper">
+          <img src={Brilliance} alt="" />
+        </div>
+        {/* Name  */}
+        <div className="statue-peer-name-wrapper">
+          <a href="" className="statue-peer-name">
+            Luke McIlvenny
+          </a>
+        </div>
+        {/* The X button  */}
+      </div>
+
+      {/* peer  */}
+      <div className="statue-peer">
+        {/* Image  */}
+        <div className="statue-peer-img-wrapper">
+          <img src={Brilliance} alt="" />
+        </div>
+        {/* Name  */}
+        <div className="statue-peer-name-wrapper">
+          <a href="" className="statue-peer-name">
+            Luke McIlvenny
+          </a>
+        </div>
+        {/* The X button  */}
+      </div>
+
+      {/* peer  */}
+      <div className="statue-peer">
+        {/* Image  */}
+        <div className="statue-peer-img-wrapper">
+          <img src={Brilliance} alt="" />
+        </div>
+        {/* Name  */}
+        <div className="statue-peer-name-wrapper">
+          <a href="" className="statue-peer-name">
+            Luke McIlvenny
+          </a>
+        </div>
+        {/* The X button  */}
+      </div>
+      </div>
 
     </div>
   );

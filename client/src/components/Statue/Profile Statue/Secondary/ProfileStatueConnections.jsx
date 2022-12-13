214832b162dbedec60e756ca1c39bb278efc2a1a
Nate <itsnzte@gmail.com>
2022-12-12 17:17:18 -0800

    made the social links section it's own component

---
+++ client/src/components/Statue/Profile Statue/Secondary/ProfileStatueConnections.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useRef } from "react";
+
+// Proptypes 
+import PropTypes from "prop-types";
+
+const ProfileStatueConnections = () => {
+  return (
+    <div>ProfileStatueConnections</div>
+  );
+};
+
+export default ProfileStatueConnections;
--- client/src/components/Statue/Profile Statue/Secondary/index.js
+++ client/src/components/Statue/Profile Statue/Secondary/index.js
@@ -1,9 +1,13 @@
 // Import 
+import ProfileStatueSocial from "./ProfileStatueSocial";
 import ProfileStatueSecondary from "./ProfileStatueSecondary";
 import ProfileStatueSocialLink from "./ProfileStatueSocialLink";
+import ProfileStatueConnections from "./ProfileStatueConnections";
 
 // Export 
 export {
+  ProfileStatueSocial,
   ProfileStatueSecondary,
   ProfileStatueSocialLink,
+  ProfileStatueConnections,
 };

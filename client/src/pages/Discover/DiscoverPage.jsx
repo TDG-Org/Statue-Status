d0d9182dd64041bfff66e9680a258750e0f922d7
Nate <itsnzte@gmail.com>
2022-12-15 21:04:25 -0800

    added a discovery item

--- client/src/pages/Discover/DiscoverPage.jsx
+++ client/src/pages/Discover/DiscoverPage.jsx
@@ -15,8 +15,8 @@ const DiscoverPage = () => {
 
       <hr />
 
+      <h3>Exploration And Connection</h3>
       <div className="discover-content">
-
         {/* Discovering Search Section  */}
         <div className="discover-search-sect pallet">
           <div className="container">
@@ -24,15 +24,32 @@ const DiscoverPage = () => {
 
             {/* Search Tool  */}
             <div className="discover-search-tool">
-            <input type="text" className="search-bar" placeholder="John Doe" />
+               <input type="text" className="search-bar" placeholder="John Doe" />
               <button className="search-bar-btn"><i className="bi-search"></i></button>
             </div>
 
             {/* Search Results  */}
+            <div className="discover-search-results-label">
+              
+            </div>
             <div className="discover-search-results">
+
+              {/* result  */}
               <div className="discover-result">
-                <p>Lorem ipsum dolor sit amet.</p>
+                <div className="discover-result-img-wrapper">
+                  <img src={Planet} alt="" />
+                </div>
+                <div className="discover-result-name">
+                  Christian McIlvenny
+                </div>
+                <a href="" className="discover-result-statue-link">
+                  Christian NathanielMcIlvenny
+                </a>
+                <div className="discover-result-reps">
+                  Reps: <span className="discover-result-rep">1.2k</span>
+                </div>
               </div>
+
             </div>
 
             <img src={Search} alt="search" className="search-svg" />

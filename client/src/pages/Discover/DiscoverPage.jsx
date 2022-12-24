c756ef127fcc1fe5072284e07e7d272046b6a776
Nate <itsnzte@gmail.com>
2022-12-24 08:13:54 -0800

    small adjustments

--- client/src/pages/Discover/DiscoverPage.jsx
+++ client/src/pages/Discover/DiscoverPage.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 // Styles 
 import "./DiscoverPage.scss";
 
 // Images/SVGs 
 import { Planet, Male, Pancake, Bravery, Brilliance } from "../../assets/imgs";
-import Search from "../../assets/svgs/search.svg";
 
 // Components 
 import { Explore, Friends } from "../../components";
@@ -14,19 +13,16 @@ const DiscoverPage = () => {
 
   return (
     <div className="DiscoverPage page">
-
       <h1>Discover</h1>
-
       <hr />
-
+      
       <h3>Exploration And Connection</h3>
       <div className="discover-content">
-        {/* Discovering Search Section  */}
 
+        {/* Discovering Search Section  */}
         <Explore />
 
         {/* User's Friends  */}
-
         <Friends />
 
       </div>

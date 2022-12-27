c62b74298dcfb32f6194cb6ec3d442da10858924
Nate <itsnzte@gmail.com>
2022-12-26 19:25:41 -0800

    added another page for the richest people, and link scss file

---
+++ client/src/pages/Richest/RichestPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Styles 
+import "./PeoplePage.scss";
+
+// Images/SVGs 
+
+const RichestPage = () => {
+  return (
+    <div className="RichestPage page">
+      <h1>Richest People</h1>
+      <hr />
+
+      {/* Content  */}
+    </div>
+  );
+};
+
+export default RichestPage;

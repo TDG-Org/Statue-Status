4e87dd20cc4e7d356acb04b871f6194d50ee3738
Nate <itsnzte@gmail.com>
2022-12-26 18:39:38 -0800

    filling new component with content, and relinking image

--- client/src/components/About/Starter.jsx
+++ client/src/components/About/Starter.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
+// Link 
+import { Link } from "react-router-dom";
+
+// Images/SVGs 
+import DevProcess from "../../assets/svgs/development-process.svg";
+
 const Starter = () => {
   return (
-    <div>Starter</div>
+    <div className="about-sect" id="getStartedSect">
+
+      <h2>Lets Get Started</h2>
+
+      {/* Content  */}
+      <div className="about-sect-content">
+
+        {/* Image  */}
+        <img src={DevProcess} alt="Getting Started" />
+
+        <p>To get started on Statue Status, create an account and set up your profile by uploading your avatar and filling out your bio. You can then discover other users on the <Link to="/discover" className="about-link">Discover</Link> page, or visit the <Link to="/" className="about-link">Home</Link> page to see our dynamic charts showcasing the richest and most powerful people, as well as those who are trending. Create your own <a href="#statueSect" className="about-link">Statue</a> to showcase your professional online presence and share it with others using a single link. Don&apos;t forget to visit the gamification page to earn XP and gems as you engage with the app. Enjoy your time on Statue Status!</p>
+      </div>
+      
+   </div>
   );
 };
 
--- client/src/pages/About/AboutPage.jsx
+++ client/src/pages/About/AboutPage.jsx
@@ -23,13 +23,6 @@ const AboutPage = () => {
 
         {/* Getting Started  */}
         <Starter />
-        <div className="about-sect" id="getStartedSect">
-          <h2>Lets Get Started</h2>
-          <div className="about-sect-content">
-            <img src={DevProcess} alt="" />
-            <p>To get started on Statue Status, create an account and set up your profile by uploading your avatar and filling out your bio. You can then discover other users on the <Link to="/discover" className="about-link">Discover</Link> page, or visit the <Link to="/" className="about-link">Home</Link> page to see our dynamic charts showcasing the richest and most powerful people, as well as those who are trending. Create your own <a href="#statueSect" className="about-link">Statue</a> to showcase your professional online presence and share it with others using a single link. Don&apos;t forget to visit the gamification page to earn XP and gems as you engage with the app. Enjoy your time on Statue Status!</p>
-            </div>
-        </div>
 
         {/* What are Statues? */}
         <div className="about-sect" id="statuesSect">

8e7d0062bbff5ea96008593cce67fa94dd668f58
Nate <itsnzte@gmail.com>
2022-12-26 19:19:58 -0800

    added people list page

---
+++ client/src/pages/People/PeoplePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Styles 
+import "./PeoplePage.scss";
+
+// Images/SVGs 
+
+const PeoplePage = () => {
+  return (
+    <div>PeoplePage</div>
+  );
+};
+
+export default PeoplePage;

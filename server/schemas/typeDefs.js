02cb682ec8e1c1fa4be4094859686223f34665e9
Avni <avni0000patel@gmail.com>
2022-12-28 18:49:25 -0500

    edit addStatueBio and addStatueName

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -80,8 +80,8 @@ const typeDefs = gql`
     editProfileBio(bio: String!): Profile
     editProfileAvatar(avatar: String!): Profile
     removeProfile(profileId: ID!): Profile
-    addStatueName(profileId: ID!, name: String): Statue
-    addStatueBio(profileId: ID!, bio: String): Statue
+    addStatueName(profileId: ID!, name: String): Profile
+    addStatueBio(profileId: ID!, bio: String): Profile
     addHeadline(headline: String): Accessory
     addBirthday(birthday: String): Accessory
     addLocation(location: String): Accessory

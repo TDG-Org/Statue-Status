780dbf82adde7e3532f991d107d80ce24faa8faa
Avni <avni0000patel@gmail.com>
2022-12-29 17:42:19 -0500

    addCustomizationPrimaryColor Mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -317,6 +317,23 @@ const resolvers = {
             }
             throw new AuthenticationError("You need to be logged in!");
         },
+        addCustomizationPrimaryColor: async (parent, { profileId, primaryColor }, context) => {
+            if (context.user) {
+                return Profile.findOneAndUpdate(
+                    { _id: profileId },
+                    {
+                        $addToSet: {
+                            customizations: { primaryColor },
+                        },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        },
         removeProfile: async (parent, { profileId }, context) => {
             if (context.user) {
                 const profile = await Profile.findOneAndDelete({

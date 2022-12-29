52625d021f61d4ed2234901a553d826edbb764b4
Avni <avni0000patel@gmail.com>
2022-12-28 20:25:58 -0500

    addRelationship Mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -198,6 +198,23 @@ const resolvers = {
             }
             throw new AuthenticationError("You need to be logged in!");
         },
+        addRelationship: async (parent, { profileId, relationship }, context) => {
+            if (context.user) {
+                return Profile.findOneAndUpdate(
+                    { _id: profileId },
+                    {
+                        $addToSet: {
+                            accessories: { relationship },
+                        },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        },
         removeProfile: async (parent, { profileId }, context) => {
             if (context.user) {
                 const profile = await Profile.findOneAndDelete({

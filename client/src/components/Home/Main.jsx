eaf6c8a9f4ec31e97f2f32f9076f05a52cc19e17
Nate <itsnzte@gmail.com>
2022-12-29 14:21:11 -0800

    created main component to modularize the home page

---
+++ client/src/components/Home/Main.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+// Components
+import {
+  Pie,
+  Chart,
+} from "./";
+
+// Link 
+import { Link } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>Main</div>
+  );
+};
+
+export default Main;
--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 import {
   Pie,
   Chart,
-  RichList,
 } from "../../components";
 
 // Link 

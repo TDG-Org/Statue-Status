4c3539367b04d5087ae1290c4fbbad5acf8895e8
Nate <itsnzte@gmail.com>
2022-12-29 21:31:56 -0800

    added typing text to welcome users

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -5,6 +5,27 @@ import "./LoginPage.scss";
 
 const LoginPage = () => { 
 
+  // Typing Text
+  const content = "Welcome to Statue Status";
+  const [text, setText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText(prevText => {
+        if (prevText.length === content.length) {
+          clearInterval(interval);
+          return prevText;
+        }
+        return content.slice(0, prevText.length + 1);
+      });
+      setIndex(prevIndex => prevIndex + 1);
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
+  // Fading in Elements 
+
   const elements = [
     { element: <div className="li-block">Element 1</div>, id: 1 },
     { element: <div className="li-block">Element 2</div>, id: 2 },
@@ -30,6 +51,7 @@ const LoginPage = () => {
 
       <h1>Login</h1>
       <hr />
+      {text}
 
       {elements.map((element, index) => (
         React.cloneElement(element.element, {

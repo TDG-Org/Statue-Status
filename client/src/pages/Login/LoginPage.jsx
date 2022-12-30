17c777b23955c3ae2d4b1b6a06643962d023f908
Nate <itsnzte@gmail.com>
2022-12-29 21:47:44 -0800

    added typing text with blinking cursor that stops 1.5s after it's done

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -6,23 +6,48 @@ import "./LoginPage.scss";
 const LoginPage = () => { 
 
   // Typing Text
+
   const content = "Welcome to Statue Status";
+
   const [text, setText] = useState("");
   const [index, setIndex] = useState(0);
+  const [showCursor, setShowCursor] = useState(true);
+  const [typingDone, setTypingDone] = useState(false);
+  const [keepBlinking, setKeepBlinking] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setText(prevText => {
         if (prevText.length === content.length) {
-          clearInterval(interval);
+          setTypingDone(true);
           return prevText;
         }
         return content.slice(0, prevText.length + 1);
       });
       setIndex(prevIndex => prevIndex + 1);
-    }, 100);
+    }, 50);
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    if (!keepBlinking) {
+      setShowCursor(false);
+      return;
+    }
+    const interval = setInterval(() => {
+      setShowCursor(prevShowCursor => !prevShowCursor);
+    }, 500);
+    return () => clearInterval(interval);
+  }, [keepBlinking]);
+
+  useEffect(() => {
+    if (typingDone) {
+      setTimeout(() => {
+        setKeepBlinking(false);
+      }, 1500);
+    }
+  }, [typingDone]);
+
 
   // Fading in Elements 
 
@@ -51,7 +76,9 @@ const LoginPage = () => {
 
       <h1>Login</h1>
       <hr />
-      {text}
+      <p>{text} {showCursor && <span className="cursor">|</span>}</p>
+      
+      
 
       {elements.map((element, index) => (
         React.cloneElement(element.element, {

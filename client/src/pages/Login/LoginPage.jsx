48219821e3265aa6fbb66127f0cb2462a558a076
Nate <itsnzte@gmail.com>
2022-12-29 17:04:52 -0800

    set up login page

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+// Styles
+import "./LoginPage.scss";
+
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="LoginPage page">
+      <h1>Login</h1>
+      <hr />
+
+      <div className="container">
+
+      </div>
+
+    </div>
   );
 };
 

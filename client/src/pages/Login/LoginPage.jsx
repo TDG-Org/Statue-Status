46dba5dbd85c40fb23642df9477ece933fbd5fc5
Nate <itsnzte@gmail.com>
2022-12-29 20:20:52 -0800

    adding fade in styles

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -5,13 +5,26 @@ import "./LoginPage.scss";
 
 const LoginPage = () => { 
 
+  const [opacity, setOpacity] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOpacity(1);
+    }, 1500);
+  }, []);
+
   return (
     <div className="LoginPage page">
-      <div className="login-page-overlay-background">
+
+      <h1>Login</h1>
+      <hr />
+
+      <div style={{ opacity, transition: "opacity 1s" }} className="container">
+        <div className="pallet">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, culpa quaerat. Fugit odit numquam molestiae debitis, adipisci necessitatibus exercitationem ex nam ipsam dignissimos quas beatae quod, reprehenderit hic officiis tempore!
+        </div>
       </div>
-      <div className="login-page-overlay-background2">
-      </div>
-     <h1>Login</h1>
+
     </div>
   );
 };

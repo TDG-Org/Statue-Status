95107b388ba85cd4df2a61ac36c8ddbfaafc426e
Nate <itsnzte@gmail.com>
2022-12-30 00:15:40 -0800

    setting up the sign up component

--- client/src/pages/SignUp/SignUpPage.jsx
+++ client/src/pages/SignUp/SignUpPage.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// Styles
+import "./SignUpPage.scss";
+
+// Sweet Alert 
+import swal from "sweetalert";
+
+// Link 
+import { Link } from "react-router-dom";
+
+// Components 
+import {
+  Flares,
+  TypingText
+} from "../../components";
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className="SignUpPage page">
+      <h1>SignUpPage</h1>
+      <hr />
+
+    </div>
   );
 };
 

069b704f4919771a8cfaf3d87b788d47c9453d28
Nate <itsnzte@gmail.com>
2022-12-30 15:46:59 -0800

    small adjustments and cleaned code

--- client/src/pages/404/NotFoundPage.jsx
+++ client/src/pages/404/NotFoundPage.jsx
@@ -9,15 +9,17 @@ const NotFoundPage = () => {
   return (
     <div className="NotFoundPage page page-full">
 
+      {/* The Content  */}
       <div className="container">
         <div className="not-found-text-content">
           <h1>404</h1>
           <h4>Hmmm... nothing was found here</h4>
           <p>Let&apos;s take you back!</p>
-          <i className="bi bi-house-door-fill"></i>
+          <i className="bi bi-house-door-fill" title="Statue Status"></i>
         </div>
       </div>
 
+      {/* The Background  */}
       <div className="the-not-found-bg-wrapper">
         <div className="not-found-black one"></div>
         <div className="not-fount-bg "></div>

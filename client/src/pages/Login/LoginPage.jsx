1c1618818743e9520fc89279628e17aceafa1358
Nate <itsnzte@gmail.com>
2022-12-29 21:24:36 -0800

    added nice transition for elements

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -5,13 +5,25 @@ import "./LoginPage.scss";
 
 const LoginPage = () => { 
 
-  const [opacity, setOpacity] = useState(0);
+  const elements = [
+    { element: <div className="li-block">Element 1</div>, id: 1 },
+    { element: <div className="li-block">Element 2</div>, id: 2 },
+    { element: <div className="li-block">Element 3</div>, id: 3 }
+  ];
+  const [opacities, setOpacities] = useState(elements.map(() => 0));
 
   useEffect(() => {
-    setTimeout(() => {
-      setOpacity(1);
-    }, 1500);
+    elements.forEach((element, index) => {
+      setTimeout(() => {
+        setOpacities(prevOpacities => {
+          const newOpacities = [...prevOpacities];
+          newOpacities[index] = 1;
+          return newOpacities;
+        });
+      }, index * 250);
+    });
   }, []);
+
 
   return (
     <div className="LoginPage page">
@@ -19,12 +31,13 @@ const LoginPage = () => {
       <h1>Login</h1>
       <hr />
 
-      <div style={{ opacity, transition: "opacity 1s" }} className="container">
-        <div className="pallet">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, culpa quaerat. Fugit odit numquam molestiae debitis, adipisci necessitatibus exercitationem ex nam ipsam dignissimos quas beatae quod, reprehenderit hic officiis tempore!
-        </div>
-      </div>
-
+      {elements.map((element, index) => (
+        React.cloneElement(element.element, {
+          style: { opacity: opacities[index], transition: "opacity 1s" },
+          key: element.id
+        })
+      ))}
+      
     </div>
   );
 };

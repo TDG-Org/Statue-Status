d894d397eb245deeae8978f40d47f3c36cadf56e
Nate <itsnzte@gmail.com>
2023-01-01 02:39:09 -0800

    added layout for accessories

--- client/src/components/Statue/Main Statue/Secondary/Accessories.jsx
+++ client/src/components/Statue/Main Statue/Secondary/Accessories.jsx
@@ -7,10 +7,41 @@ const Accessories = () => {
   return (
     <div className="statue-accessories-sect">
 
+      {/* Headline */}
       <div className="statue-accessory statue-accessory-headline">
-        
+        <p>
+          Software Engineer
+        </p>
       </div>
 
+      {/* Birthday */}
+      <div className="statue-accessory statue-accessory-birthday">
+        <p>
+         Born <span> 04/08/2003 </span> - <span>19</span>
+        </p>
+      </div>
+
+      {/* Location */}
+      <div className="statue-accessory statue-accessory-location">
+        <p>
+         Perris, CA
+        </p>
+      </div>
+
+      {/* Company */}
+      <div className="statue-accessory statue-accessory-company">
+        <p>
+         @ TDG
+        </p>
+      </div>
+      
+      {/* Relationship */}
+      <div className="statue-accessory statue-accessory-company">
+        <p>
+         Single
+        </p>
+      </div>
+      
     </div>
   );
 };

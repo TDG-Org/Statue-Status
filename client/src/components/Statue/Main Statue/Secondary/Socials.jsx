dce89a02a3738523e6ccd49807cd055f5b492bc3
Nate <itsnzte@gmail.com>
2022-12-31 23:15:41 -0800

    added styles for statue social link

--- client/src/components/Statue/Main Statue/Secondary/Socials.jsx
+++ client/src/components/Statue/Main Statue/Secondary/Socials.jsx
@@ -4,8 +4,22 @@ const Socials = () => {
   return (
     <div className="statue-socials-sect">
 
+      {/* Statue Link  */}
       <div className="statue-link">
-
+        <a
+          href="#"
+          target="_blank"
+          className="statue-link-anchor"
+        >
+          {/* Icon Platform */}
+          <i className="bi bi-facebook"></i>
+          {/* Platform */}
+          Facebook/
+          {/* Username */}
+          <span className="statue-social-username">
+            TDGNate
+          </span>
+        </a>
       </div>
 
     </div>

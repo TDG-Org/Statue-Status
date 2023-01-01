d52120eddabbee7e967ede2e7d4804e6cdffea0c
Nate <itsnzte@gmail.com>
2023-01-01 15:31:25 -0800

    adjusted accessories and added icons

--- client/src/components/Statue/Main Statue/Secondary/Accessories.jsx
+++ client/src/components/Statue/Main Statue/Secondary/Accessories.jsx
@@ -9,6 +9,7 @@ const Accessories = () => {
 
       {/* Headline */}
       <div className="statue-accessory statue-accessory-headline">
+        <i className="bi bi-card-heading"></i>
         <p>
           Software Engineer
         </p>
@@ -16,6 +17,7 @@ const Accessories = () => {
 
       {/* Birthday */}
       <div className="statue-accessory statue-accessory-birthday">
+        <i className="bi bi-calendar-event"></i>
         <p>
          Born <span> 04/08/2003 </span> - <span>19</span>
         </p>
@@ -23,6 +25,7 @@ const Accessories = () => {
 
       {/* Location */}
       <div className="statue-accessory statue-accessory-location">
+        <i className="bi bi-geo-alt"></i>
         <p>
          Perris, CA
         </p>
@@ -30,6 +33,7 @@ const Accessories = () => {
 
       {/* Company */}
       <div className="statue-accessory statue-accessory-company">
+        <i className="bi bi-building"></i>
         <p>
          @ TDG
         </p>
@@ -37,6 +41,7 @@ const Accessories = () => {
       
       {/* Relationship */}
       <div className="statue-accessory statue-accessory-company">
+        <i className="bi bi-heart-fill"></i>
         <p>
          Single
         </p>

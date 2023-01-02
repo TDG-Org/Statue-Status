0564b680f0fdbbb94ec5c37be8b428e13b4bc296
Nate <itsnzte@gmail.com>
2023-01-01 16:30:10 -0800

    fixed order of elements and fixing styles for mobile screens

--- client/src/components/Statue/Main Statue/ClientDetails.jsx
+++ client/src/components/Statue/Main Statue/ClientDetails.jsx
@@ -18,19 +18,19 @@ const ClientDetails = () => {
     <div className="statue-details-comp">
 
       {/* The User's Accossories  */}
-      <div className="statue-detail">
+      <div className="statue-detail statue-detail-accessory">
         <h5>Christian</h5>
         <Accessories />
       </div>
 
       {/* The User's Socials Links  */}
-      <div className="statue-detail">
+      <div className="statue-detail statue-detail-social">
         <h5>Socials</h5>
         <Socials />
       </div>
 
       {/* The User's Connections  */}
-      <div className="statue-detail">
+      <div className="statue-detail statue-detail-connection">
         <h5>Connections</h5>
         <Connections />
       </div>

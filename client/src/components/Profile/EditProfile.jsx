ac465b17c96dbd542349247580515dff72c8e515
Nate <itsnzte@gmail.com>
2023-01-04 02:19:01 -0800

    fixed profile sections spacing

--- client/src/components/Profile/EditProfile.jsx
+++ client/src/components/Profile/EditProfile.jsx
@@ -18,16 +18,23 @@ const EditProfile = () => {
     <div className="profile-sect">
       <h3>Edit Profile</h3>
         <div className="pallet">
-          <div className="container">
-            <div className="user-edit">
+        <div className="container">
+          
+          <div className="user-edit">
+            
               {/* Avatar  */}
-              <Avatar />
+            <Avatar />
+            
               {/* Right Side  */}
-              <Middle />
-            </div>
-            <div className="profile-sect-divider"></div>
+            <Middle />
+            
+          </div>
+          
+          <div className="profile-sect-divider"></div>
+          
             {/* User info  */}
-            <UserInfo />
+          <UserInfo />
+          
           </div>
         </div>
     </div>

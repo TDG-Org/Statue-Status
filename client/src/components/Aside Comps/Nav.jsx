d77319d4354a3ae0e65ad50773d24b20594e6cd2
Avni <avni0000patel@gmail.com>
2023-01-06 11:06:33 -0500

    logout functionality set up

--- client/src/components/Aside Comps/Nav.jsx
+++ client/src/components/Aside Comps/Nav.jsx
@@ -9,6 +9,8 @@ import { Link } from "react-router-dom";
 // Props 
 import PropTypes from "prop-types";
 
+import Auth from "../../utils/auth";
+
 const Nav = ({ isActive, handleToggle }) => {
 
   // Validate Prop 
@@ -16,91 +18,141 @@ const Nav = ({ isActive, handleToggle }) => {
     isActive: PropTypes.bool.isRequired,
     handleToggle: PropTypes.func.isRequired
   };
-  
+
+  function logoutFunc(event) {
+    event.preventDefault();
+    Auth.logout;
+  }
+
   return (
     <nav className={`nav-comp ${isActive ? "" : "active"}`}>
 
       {/* Personal Section */}
-      
-      <ul className="nav-sect">
 
-        {/* Home */}
-        <li>
-          <Link
-            to="/"
-            onClick={handleToggle}
-            className="ss-nav-link ss-active"
-          >
-            Home
-          </Link>
-        </li>
+      {Auth.loggedIn() ? (
+        <>
+          <ul className="nav-sect">
+            <li>
+              <Link
+                to="/"
+                onClick={handleToggle}
+                className="ss-nav-link ss-active"
+              >
+                Home
+              </Link>
+            </li>
 
-        {/* Profile */}
-        <li>
-          <Link
-            to="/profile"
-            onClick={handleToggle}
-            className="ss-nav-link"
-          >
-            Profile
-          </Link>
-        </li>
+            {/* Profile */}
+            <li>
+              <Link
+                to="/profile"
+                onClick={handleToggle}
+                className="ss-nav-link"
+              >
+                Profile
+              </Link>
+            </li>
 
-        {/* Discover */}
-        <li>
-          <Link
-            to="/discover"
-            onClick={handleToggle}
-            className="ss-nav-link"
-          >
-            Discover
-          </Link>
-        </li>
+            {/* Discover */}
+            <li>
+              <Link
+                to="/discover"
+                onClick={handleToggle}
+                className="ss-nav-link"
+              >
+                Discover
+              </Link>
+            </li>
 
-      </ul>
+          </ul>
 
-      <hr className="aside-divider" />
+          <hr className="aside-divider" />
 
-      {/* General Section */}
+          {/* General Section */}
 
-      <ul className="ss-nav-sect">
+          <ul className="ss-nav-sect">
 
-        {/* Settings */}
-        <li>
-          <Link
-            to="/settings"
-            onClick={handleToggle}
-            className="ss-nav-link"
-          >
-            Settings
-          </Link>
-        </li>
+            {/* Settings */}
+            <li>
+              <Link
+                to="/settings"
+                onClick={handleToggle}
+                className="ss-nav-link"
+              >
+                Settings
+              </Link>
+            </li>
 
-        {/* Learn More */}
-        <li>
-          <Link
-            to="/about"
-            onClick={handleToggle}
-            className="ss-nav-link"
-          
-          >Learn More
-          </Link>
-        </li>
+            {/* Learn More */}
+            <li>
+              <Link
+                to="/about"
+                onClick={handleToggle}
+                className="ss-nav-link"
 
-        {/* Logout */}
-        <li>
-          <a
-            href="#"
-            className="ss-nav-link"
-            id="logoutBtn"
-          >
-            Logout <i className="bi bi-box-arrow-in-right"></i>
-          </a>
-        </li>
+              >Learn More
+              </Link>
+            </li>
 
-      </ul>
+            {/* Logout */}
+            <li>
+              <a
+                href="#"
+                className="ss-nav-link"
+                id="logoutBtn"
+                onClick={logoutFunc}
+              >
+                Logout <i className="bi bi-box-arrow-in-right"></i>
+              </a>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <ul className="nav-sect">
+            <li>
+              <Link
+                to="/"
+                onClick={handleToggle}
+                className="ss-nav-link ss-active"
+              >
+                Home
+              </Link>
+            </li>
 
-    </nav>
+          </ul>
+
+          <hr className="aside-divider" />
+
+          {/* General Section */}
+
+          <ul className="ss-nav-sect">
+
+            {/* Learn More */}
+            <li>
+              <Link
+                to="/about"
+                onClick={handleToggle}
+                className="ss-nav-link"
+
+              >Learn More
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/login"
+                className="ss-nav-link"
+              >
+                Login
+              </Link>
+            </li>
+
+          </ul>
+        </>
+      )}
+
+    </nav >
   );
 };
 

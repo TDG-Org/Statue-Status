628f955b523164aa64a70403bf213b7025f2e138
Nate <itsnzte@gmail.com>
2023-01-10 17:13:54 -0800

    able to switch picture in statue

--- client/src/components/Statue/Profile Statue/Primary/ProfileStatuePrimary.jsx
+++ client/src/components/Statue/Profile Statue/Primary/ProfileStatuePrimary.jsx
@@ -3,21 +3,31 @@ import React, { useState, useRef } from "react";
 import { ProfileStatueAccessory, ProfileStatueBages, ProfileStatueAbout } from "./";
 
 // Images/SVGs 
-import { Pancake, Brilliance, Bravery, NatePfp } from "../../../../assets/imgs";
+import { NatePfp } from "../../../../assets/imgs";
 
 const ProfileStatuePrimary = () => {
 
-  const statueNameRef = useRef(null);
+  // Statue Image 
+  const [statueImage, setStatueImage] = useState(null);
+  const inputStatueImageRef = useRef();
+  const StatueImageRef = useRef();
 
+  const handleStatueImageChange = (event) => {
+    setStatueImage(event.target.files[0]);
+    StatueImageRef.current.src = URL.createObjectURL(event.target.files[0]);
+  };
+
+  const handleStatueImageInputClick = () => {
+    inputStatueImageRef.current.click();
+  };
+
+  const statueNameRef = useRef(null);
   // Check if Editing is active
   const [editStatueNameActive, setEditStatueNameActive] = useState(false);
-  
   // Official Name 
   const [editStatueName, setEditStatueName] = useState("Christian McIlvenny");
-
   // Current Name input 
   const [statueNameCurrent, setStatueNameCurrent] = useState(editStatueName);
-  
   // Toggle function to activate Statue Name edit
   function handleToggleStatueName(e) {
     setEditStatueNameActive(!editStatueNameActive);
@@ -25,19 +35,16 @@ const ProfileStatuePrimary = () => {
       statueNameRef.current.focus();
     }, 50);
   } 
-
   // Update the input text 
   function updateStatueName(e) {
     setEditStatueName(statueNameCurrent);
   }
-
   // Function that always listens for input changes 
   function handleStatueNameInputChange(e) {
     const target = e.target;
     let value = target.value;
     setStatueNameCurrent(value);
   }
-
   // Update the display 
   function displayStatueName() {
     document.querySelector(".statue-name").value = editStatueName;
@@ -47,13 +54,23 @@ const ProfileStatuePrimary = () => {
     <div className="primary-sect">
               
       {/* Statue Picture */}
-      <div className="statue-img">
-        {/* overlay */}
+      <div
+        onClick={handleStatueImageInputClick}
+        className="statue-img"
+      >
+        {/* overlay  */}
         <div className="statue-img-overlay"></div>
         {/* image */}
         <img
+          ref={StatueImageRef}
           src={NatePfp}
           alt="Statue Picture"
+        />
+        <input
+          hidden
+          type="file"
+          ref={inputStatueImageRef}
+          onChange={handleStatueImageChange}
         />
       </div>
 

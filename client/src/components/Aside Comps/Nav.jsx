49005342cb8cc05622aa19a7438ee0cedb541f9f
Nate <itsnzte@gmail.com>
2023-01-10 19:51:52 -0800

    added auth into user component and added a few adjustments

--- client/src/components/Aside Comps/Nav.jsx
+++ client/src/components/Aside Comps/Nav.jsx
@@ -11,6 +11,7 @@ import PropTypes from "prop-types";
 
 import Auth from "../../utils/auth";
 
+// Auth 
 const Nav = ({ isActive, handleToggle }) => {
 
   // Validate Prop 
--- client/src/components/Aside Comps/User.jsx
+++ client/src/components/Aside Comps/User.jsx
@@ -9,14 +9,17 @@ import { Link } from "react-router-dom";
 // Images/SVGs 
 import { Bravery, Brilliance, Pancake } from "../../assets/imgs";
 
+// Auth 
+import Auth from "../../utils/auth";
+
 const User = () => {
   return (
     <div className="user-avatar-section">
 
-      {/* Top Section  */}
+      {/* Top Section */}
       <div className="user-avatar-sect-top">
 
-      {/* badge section  */}
+      {/* badge section */}
       <ul className="badge-sect">
         <li className="badge">
             <a href="#">
@@ -30,17 +33,17 @@ const User = () => {
         </li>
       </ul>
 
-      {/* Avatar Section  */}
+      {/* Avatar Section */}
       <Link to="/profile" className="avatar-sect">
           <img src={Pancake} alt="" />   
       </Link>
 
       </div>
 
-      {/* Bottom Section  */}
+      {/* Bottom Section */}
       <div className="user-avatar-sect-bottom">
 
-        {/* Username  */}
+        {/* Username */}
         <h3 className="username"><span className="the-at">@</span><a id="username">Nate</a></h3>
 
         <div className="user-data">
@@ -52,18 +55,18 @@ const User = () => {
           </div>
         </div>
         
-        {/* User bio section  */}
+        {/* User bio section */}
         <p className="user-bio" id="userBio">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam saepe, dolor dolorem pariatur odit rem?
         </p>
 
-        {/* User's Followers and Following Count  */}
+        {/* User's Followers and Following Count */}
         <div className="follow-count-wrapper">
           <p className="following-count"><span id="userFollowersCount">287m</span> Followers</p>
           <p className="following-count"><span id="userFollowingCount">1</span> Following</p>
         </div>
 
-        {/* Links (Future)  */}
+        {/* Links (Future) */}
 
       </div>
 
